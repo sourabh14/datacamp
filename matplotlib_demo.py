9ffa1421ce5209db7e4308833336235d1c2ac71a
Sourabh Shrivastava <soshriva@adobe.com>
2017-05-31 20:41:03 +0530

    initial commit

---
+++ matplotlib_demo.py
@@ -0,0 +1,11 @@
+# matplotlib demo
+import matplotlib.pyplot as plt
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+
+# line plot
+plt.plot(year, pop)
+
+# point plot
+#plt.scatter(year, pop)
+plt.show()
