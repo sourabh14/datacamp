6fdf97c68c0d82c04212af961a033253f672b497
Sourabh Shrivastava <soshriva@adobe.com>
2017-06-02 10:18:33 +0530

    Add to matplotlib

--- 3_matplotlib_demo.py
+++ 3_matplotlib_demo.py
@@ -1,7 +1,10 @@
 # matplotlib demo
 import matplotlib.pyplot as plt
+
 year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
+
 '''
 # line plot
 # useful when x axis represents time
@@ -15,13 +18,19 @@ plt.xscale('log')
 
 # show plot
 plt.show()
+'''
 
 # Histogram
 # arguments
 # $1 - list of values 
 # $2 - bins (values are divided into bins)
-'''
 
-values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
+# min val = 0, max val = 6, bin = 3
+# the histogram will be of interval 2
 plt.hist(values, bins=3)
+plt.show()
+# clf() cleans plot
+plt.clf()
+
+plt.hist(values, bins=6)
 plt.show()
